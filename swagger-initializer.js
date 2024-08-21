3e606e2f7f3b2162dc67f8366a52de6d765703fe
SeongHun Kim <61442302+kimsh2948@users.noreply.github.com>
2024-08-21 21:04:22 +0900

    Update swagger-initializer.js

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://raw.githubusercontent.com/kimsh2948/auction-swagger-ui/master/snapcraft.yaml",
+    url: "https://raw.githubusercontent.com/kimsh2948/auction-swagger-ui/master/swagger.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
